c62e3e9ed6dd178aa3e2ff78827df14d00efb182
Rafiu_Hossain <hossainrafiu@gmail.com>
2021-09-20 12:28:27 -0400

    Changes to p7

--- lab/lab02/p7.py
+++ lab/lab02/p7.py
@@ -4,15 +4,15 @@ def display_current_value():
 
 def add(to_add):
     global current_value
-    global prev_value
-    prev_value = current_value
+    global past_value
+    past_value = current_value
     current_value += to_add
 
 
 def multiply(to_multiply):
     global current_value
-    global prev_value
-    prev_value = current_value
+    global past_value
+    past_value = current_value
     current_value *= to_multiply
 
 
@@ -22,32 +22,38 @@ def subtract(to_subtract):
 
 def divide(to_divide):
     # issue: zero division
-    multiply(1 / to_divide)
+    if to_divide == 0:
+        print("ERROR! Cannot divide by zero.")
+    else:
+        multiply(1 / to_divide)
 
 
 def memory():
     global current_value
+    global memory_value
 
-    with open("test.txt", "w") as f:
-        f.write(str(current_value))
+    memory_value = current_value
 
 
 def recall():
     global current_value
+    global memory_value
 
-    with open("test.txt", "r") as f:
-        current_value = int(f.read())
+    current_value = memory_value
 
 
 def undo():
     global current_value
-    global prev_value
-    current_value, prev_value = prev_value, current_value
+    global past_value
+    temp = current_value
+    current_value = past_value
+    past_value = temp
 
 
 if __name__ == "__main__":
     current_value = 0
-    prev_value = 0
+    memory_value = 0
+    past_value = 0
     display_current_value()  # 0
     add(5)  # 5
     subtract(2)
