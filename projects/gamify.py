1df7e0d71c1ef00404713f99735782a04271ea9a
Satvick Acharya <satvickacharya16@gmail.com>
2021-10-12 09:51:55 -0400

    added lec 13 and project 1 code

--- projects/gamify.py
+++ projects/gamify.py
@@ -1,142 +1,245 @@
 def get_cur_hedons():
-    '''Return the number of hedons that 
-    the user has accumulated so far.'''
+    '''Return the number of hedons that the user has accumulated so far.
+    '''
     global hedons
     return hedons
 
 
 def get_cur_health():
-    '''Return the number of health points 
-    that the user has accumulated so far.'''
+    '''Return the number of health points that the user has accumulated so far.
+    '''
     global health
     return health
 
 
-def offer_star(activity):
-    '''Simulate offering a star for 
-    engaging in the exercise activity.'''
+def star_can_be_taken(activity):
+    '''Return True iff a star can be used to get more hedons for activity 
+    activity. A star can only be taken if no time passed between the star's 
+    being offered and the activity, and the user is not bored with the stars, 
+    and the star was offered for activity activity.
+    '''
     global star
+    global time
+    global uninterested
+
+    # check if the user has lost interest in stars and whether a star for
+    # activity was just offered
+    if len(star) > 0:
+        if star[-1] == [time, activity]:
+            if not uninterested:
+                return True
+
+    return False
+
+
+def offer_star(activity):
+    '''Simulate offering a star for engaging in the exercise activity.
+    If three or more stars have beeen offered in the past three hours, set 
+    uninterested to True to prevent any future stars from yielding bonus hedons.
+    '''
+    global star
+    global uninterested
 
     star.append([time, activity])
 
+    # if the user has not already lost interest in stars,
+    # check if 3 stars have been offered in the past 2 hrs
+    if not uninterested:
+        counter = 0
+
+        for i in reversed(star):
+            if time - star[0][0] < 120:
+                counter += 1
+
+        if counter >= 3:
+            uninterested = True
+
 
 def is_tired():
+    '''Return True iff running or textbooks has been performed in the last 120 
+    mins, according to the activity_record.
+    '''
     global activity_record
 
     for i in reversed(activity_record):
-        if time - i[1] < 120:
+        if time - i[2] < 120:
             if i[0] == "running" or i[0] == "textbooks":
                 return True
 
+    return False
+
+
+def minutes_ran_before():
+    '''If the last activity performed was running, return the total time ran.
+    Keep iterating through the activity record backwards until a time slot was not spent running.
+
+    For instance:
+    activity_record = [["resting", 30, 30], ["running", 60, 90]]
+    minutes_ran_before() -> 60
+
+    activity_record = [["resting", 30, 30], ["running", 60, 90], ["running", 20, 110]]
+    minutes_ran_before() -> 80 (60 + 20)
+    '''
+    global activity_record
+
+    total_time = 0
+
+    for i in reversed(activity_record):
+        if i[0] == "running":
+            total_time += i[1]
+        else:
+            break
+
+    return total_time
+
 
 def perform_activity(activity, duration):
-    '''Simulate the user performing 
-    activity for duration minutes.'''
+    '''Simulate the user performing activity for duration minutes.
+    Allocate health points and hedons accordingly (based on tiredness, stars 
+    and duration). Finally, add the activity to the activity_record.
+    '''
     global health
     global hedons
     global time
-    global star
+    global activity_record
 
     if activity == "running":
+        # check if using star
+        using_star = star_can_be_taken("running")
+
         # allocate health points
-        if duration > 180:
-            health += 180 * 3
-            health += duration - 180
-        else:
-            health += 3 * duration
+        already_ran = 0
+
+        if len(activity_record) > 0:
+            # if the last activity was running, minutes ran in this time slot
+            # will be added onto those already ran
+            if activity_record[-1][0] == "running":
+                already_ran = minutes_ran_before()
+
+        for minute in range(1, duration + 1):
+            if minute + already_ran > 180:
+                health += 1
+            else:
+                health += 3
 
         # allocate hedons
-        if is_tired():
-            hedons -= 2 * duration
-        else:
-            if duration > 10:
-                hedons += 2 * duration
-                hedons -= 2 * (duration - 10)
+        tired = is_tired()
+
+        for minute in range(1, duration + 1):
+            if tired:
+                hedons -= 2
             else:
-                hedons += 2 * duration
+                if minute > 10:
+                    hedons -= 2
+                else:
+                    hedons += 2
+
+            if using_star and minute <= 10:
+                hedons += 3
+
     elif activity == "textbooks":
+        # check if using star
+        using_star = star_can_be_taken("textbooks")
+
         # allocate health points
         health += 2 * duration
 
         # allocate hedons
-        if is_tired():
-            hedons -= 2 * duration
-        else:
-            if duration > 20:
-                hedons += duration
-                hedons -= duration - 20
+        tired = is_tired()
+
+        for minute in range(1, duration + 1):
+            if tired:
+                hedons -= 2
             else:
-                hedons += duration
+                if minute > 20:
+                    hedons -= 1
+                else:
+                    hedons += 1
+
+            if using_star and minute <= 10:
+                hedons += 3
+
     elif activity == "resting":
-        pass
+        # allocate hedons
+        if star_can_be_taken("resting"):
+            hedons += 3 * duration
     else:
         return
 
-    activity_record.append([activity, time + duration])
+    # add activity details to the activity record and increment time
+    activity_record.append([activity, duration, time + duration])
     time += duration
-
-
-def star_can_be_taken(activity):
-    '''Return True iff a star can be used 
-    to get more hedons for activity activity. 
-    A star can only be taken if no time passed 
-    between the star's being offered ans the 
-    activity, and the user is not bored with 
-    the stars, and the star was offered for 
-    activity activity.'''
-    pass
 
 
 def most_fun_activity_minute():
     '''Return the activity which would give 
     the most hedons if the person performed it 
-    for one minute at the current time.'''
-    pass
+    for one minute at the current time.
+    '''
+    running = 0
+    textbooks = 0
+    resting = 0
+
+    tired = is_tired()
+
+    # check "running" option
+    using_star = star_can_be_taken("running")
+
+    if tired:
+        running -= 2
+    else:
+        running += 2
+
+    if using_star:
+        running += 3
+
+    # check "textbooks" option
+    using_star = star_can_be_taken("textbooks")
+
+    if tired:
+        textbooks -= 2
+    else:
+        textbooks += 1
+
+    if using_star:
+        textbooks += 3
+
+    # check "resting" option
+    using_star = star_can_be_taken("resting")
+
+    if using_star:
+        resting += 3
+
+    # determine which activity yields maximum hedons
+    if running > textbooks and running > resting:
+        return "running"
+    elif textbooks > running and textbooks > resting:
+        return "textbooks"
+    else:
+        return "resting"
 
 
 def initialize():
-    '''Initializes the global variables needed for 
-    the simulation. Note: this function is incomplete, 
-    and you may want to modify it'''
+    '''Initialize the global variables needed for the simulation.
+    '''
+    # number of health points collected by the user
     global health
+    # number of hedons collected by the user
     global hedons
+    # time elapsed since beginning of simulation
     global time
+    # a list containing each activity performed, the duration and the time
+    # upon completion
     global activity_record
+    # a list containing every instance a star was offered with the time and
+    # activity for which it was offered
     global star
+    # a boolean value describing whether the user has lost interest in stars
+    global uninterested
 
     health = 0
     hedons = 0
     time = 0
     activity_record = []
     star = []
-
-
-if __name__ == "__main__":
-    initialize()
-    perform_activity("running", 30)
-    # -20 = 10 * 2 + 20 * (-2)             # Test 1
-    print(get_cur_hedons())
-    # 90 = 30 * 3                          # Test 2
-    print(get_cur_health())
-    # resting                              # Test 3
-    print(most_fun_activity_minute())
-    perform_activity("resting", 30)
-    offer_star("running")
-    # running                              # Test 4
-    print(most_fun_activity_minute())
-    perform_activity("textbooks", 30)
-    # 150 = 90 + 30*2                      # Test 5
-    print(get_cur_health())
-    # -80 = -20 + 30 * (-2)                # Test 6
-    print(get_cur_hedons())
-    offer_star("running")
-    perform_activity("running", 20)
-    # 210 = 150 + 20 * 3                   # Test 7
-    print(get_cur_health())
-    # -90 = -80 + 10 * (3-2) + 10 * (-2)   # Test 8
-    print(get_cur_hedons())
-    perform_activity("running", 170)
-    # 700 = 210 + 160 * 3 + 10 * 1         # Test 9
-    print(get_cur_health())
-    # -430 = -90 + 170 * (-2)              # Test 10
-    print(get_cur_hedons())
+    uninterested = False
